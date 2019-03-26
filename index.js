d8712a20b5f3bc4d884b113423225fe1b0ba1900
evilarceus <evilarceus@gmail.com>
2019-03-26 00:12:34 -0700

    Added a bunch of improvements, added more lines for Shaxx

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const path = require("path")
+const path = require("path");
+app.use("/ogg", express.static(path.join(__dirname, "ogg")));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get('/shaxx.json', (req, res) => res.sendFile(path.join(__dirname, "shaxx.json")));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
