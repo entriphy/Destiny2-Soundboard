8e783c4ee0ddedea098e47d219158f9519e089a0
Michael Burton <me@mikeylicio.us>
2019-12-07 19:06:39 +0000

    Adds D2 Dictionary JSON and uses this to find the right text for buttons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 app.use("/ogg", express.static(path.join(__dirname, "ogg")));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get('/dict.json', (req, res) => res.sendFile(path.join(__dirname, "dict.json")));
 app.get('/shaxx.json', (req, res) => res.sendFile(path.join(__dirname, "shaxx.json")));
 app.get('/saladin.json', (req, res) => res.sendFile(path.join(__dirname, "saladin.json")));
 app.get('/calus.json', (req, res) => res.sendFile(path.join(__dirname, "calus.json")));
